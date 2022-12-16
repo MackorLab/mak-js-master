464b6bd42ca88a13ab23cea416e4ab2a590d45a8
MackorLab <76862637+MackorLab@users.noreply.github.com>
2022-12-16 18:06:00 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+function render() {
+	const productsStore = localStorageUtil.getProducts();
+
+	headerPage.render(productsStore.length);
+	productsPage.render();	
+}
+
+spinnerPage.render();
+
+let CATALOG = [];
+
+// https://api.myjson.com/bins/jvsbu
+fetch('server/catalog.json')
+    .then(res => res.json())
+    .then(body => {
+		CATALOG = body;
+
+		setTimeout(() => {
+			spinnerPage.handleClear();
+			render();
+		}, 1000);
+    })
+    .catch(() => {
+        spinnerPage.handleClear();
+    	errorPage.render();
+    })

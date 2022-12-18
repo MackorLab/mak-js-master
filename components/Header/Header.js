6f38ab9fde3caa531d2ae476664e76f10aaa4125
MackorLab <76862637+MackorLab@users.noreply.github.com>
2022-12-18 10:46:40 +0700

    Update Header.js

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -7,7 +7,7 @@ class Header {
         const html = `
             <div class="header-container">
                 <div class="header-counter" onclick="headerPage.handlerOpenShoppingPage();">
-                    <a><img src="https://i.ibb.co/xjQM8kr/shoppingcart-to-compra-12829.png" alt="shoppingcart" border="0"></a> ${count}
+                    <a class="header-icon"><img src="https://i.ibb.co/xjQM8kr/shoppingcart-to-compra-12829.png" alt="shoppingcart" border="0"></a> ${count}
                 </div>
             </div>
         `;

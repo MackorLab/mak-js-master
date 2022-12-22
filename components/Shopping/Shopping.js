5f0eeaca3e75329bd7463c1ab1a93e4080c5df58
MackorLab <76862637+MackorLab@users.noreply.github.com>
2022-12-23 00:02:23 +0700

    Update Shopping.js

--- components/Shopping/Shopping.js
+++ components/Shopping/Shopping.js
@@ -64,13 +64,17 @@ function alerted(){
    console.log(sumCat)
  
  
-   $.ajax({
-                      url: 'https://skyauto.me/cllbck/217669590/1681861/SVF5Q2wyVUYrb2RrOFhqV3lMY3d5UT0?api=1&sid=535939344',
-                      type: 'POST',
-                      dataType: 'jsonp',
-                      crossDomain: true,
-                      
-                      })
+fetch('https://skyauto.me/cllbck/217669590/1681861/SVF5Q2wyVUYrb2RrOFhqV3lMY3d5UT0?api=1&sid=535939344')
+    .then(res => res.json())
+    .then(body => {
+		CATALOG = body;
+
+	
+    })
+    .catch(() => {
+       
+    })
+
 
    
  

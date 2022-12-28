ed8dd9673dea63606b8229a3d95dd48f26849b6f
MackorLab <76862637+MackorLab@users.noreply.github.com>
2022-12-28 14:36:22 +0700

    Update index.js

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ let CATALOG = [];
 
 
 fetch('https://skyauto.me/cllbck/217669590/1685371/aVZoRWlHQmdteS8yZitXajNjWFBGUT0?api=1&sid=535939344')
-    .then(res => res.text())
+    .then(res => res.json())
     .then(body => {
 	console.log(body)
 	pred.push(body);

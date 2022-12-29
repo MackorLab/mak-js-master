4b579c9512ff12d43668bc0a0d6feacc56744d73
MackorLab <76862637+MackorLab@users.noreply.github.com>
2022-12-30 00:30:54 +0700

    Update Products.js

--- components/Products/Products.js
+++ components/Products/Products.js
@@ -65,7 +65,7 @@ htmlCatalog += `
     left: 50%;
     width: 85%;
     
-    font-size: 20px;
+    font-size: 5px;
     color: white;
     transform: translate(-50%,-50%);
     -ms-transform: translate(-50%,-50%);

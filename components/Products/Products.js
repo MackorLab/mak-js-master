9d42493ed89321c4761f4cb39ac9f886f097abc0
MackorLab <76862637+MackorLab@users.noreply.github.com>
2022-12-29 23:00:03 +0700

    Update Products.js

--- components/Products/Products.js
+++ components/Products/Products.js
@@ -33,20 +33,117 @@ class Products {
                 activeClass = ' ' + this.classNameActive;
                 activeText = this.labelRemove;
             }
+            
+            
+            
+            
+            
+            
 
-            htmlCatalog += `
-                <li class="products-element">
-                    <span class="products-element__name">${name}</span>
-                    <img class="products-element__img" src="${img}" />
-                    <span class="products-element__price">
-                        ⚡️ ${price.toLocaleString()} ₽
-                    </span>
-                    <button class="products-element__btn${activeClass}" onclick="productsPage.handlerSetLocatStorage(this, '${id}');">
-                        ${activeText}
-                    </button>
-                </li>
-            `;
-        });
+   htmlCatalog += `
+
+
+
+<style>
+#overlay_${id}{
+    position: fixed;
+    display: none;
+    width: 100%;
+    height: 100%;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    background-color: rgba(0,0,0,0.5);
+    z-index: 2;
+    cursor: pointer;
+}
+
+#text_${id}{
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    width: 85%;
+    
+    font-size: 100px;
+    color: white;
+    transform: translate(-50%,-50%);
+    -ms-transform: translate(-50%,-50%);
+}
+</style>
+
+
+
+
+<div id="overlay_el1" onclick="off_${id}()">
+
+  <div id="text_${id}">
+  
+Put this script tag to the <head> of your page 
+
+  
+  </div>
+  
+  
+</div>
+
+<div style="padding:20px">
+  <h2>Overlay with Text</h2>
+  <button onclick="on_${id}()">Turn on overlay effect</button>
+</div>
+
+
+
+
+
+
+<li class="products-element">
+	<span class="products-element__name">${name}</span>
+	<img onclick="on_${id}()" class="products-element__img" src="${img}" />
+	<span class="products-element__price">
+		⚡️ ${price.toLocaleString()} ₽
+	</span>
+	<button class="products-element__btn${activeClass}" onclick="productsPage.handlerSetLocatStorage(this, '${id}');">
+		${activeText}
+	</button>
+</li>
+
+
+
+
+
+
+<script>
+
+function on_${id}() {
+    document.getElementById("overlay_el1").style.display = "block";
+}
+
+function off_${id}() {
+    document.getElementById("overlay_el1").style.display = "none";
+}
+
+
+</script>
+
+`;
+
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
 
         const html = `
             <ul class="products-container">

c8b9917db3718af48181e9135a27a242b4a5210c
MackorLab <76862637+MackorLab@users.noreply.github.com>
2022-12-31 01:24:19 +0700

    Update Products.js

--- components/Products/Products.js
+++ components/Products/Products.js
@@ -86,9 +86,7 @@ htmlCatalog += `
     border-radius: 0px;
     margin: 0 auto;
     max-width: 600px;
-    -webkit-box-shadow: 5px 5px 8px 8px rgba(176, 14, 204, 0.44);
-    -moz-box-shadow: 5px 5px 8px 8px rgba(176, 14, 204, 0.44);
-    box-shadow: 5px 5px 8px 8px rgba(176, 14, 204, 0.44); 
+   
 }
 .container {
     

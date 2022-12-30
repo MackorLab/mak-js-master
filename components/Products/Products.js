05e99020e9303e72ff2aafc8c4212c4c8d94bd48
MackorLab <76862637+MackorLab@users.noreply.github.com>
2022-12-30 12:00:29 +0700

    Update Products.js

--- components/Products/Products.js
+++ components/Products/Products.js
@@ -79,24 +79,52 @@ htmlCatalog += `
 
   <div id="text_${id}">
 <style>
-img.sticky${id} {
-    position: -webkit-sticky;
-    position: sticky;
-    top: 0;
-    width: 200px;
+
+
+.coupon {
+    border: 5px dotted #bbb;
+    width: 80%;
+    border-radius: 15px;
+    margin: 0 auto;
+    max-width: 600px;
+}
+
+.container {
+    padding: 2px 16px;
+    background-color: #f1f1f1;
+}
+
+.promo {
+    background: #ccc;
+    padding: 3px;
+}
+
+.expire {
+    color: red;
 }
 </style>
-</head>
-<body>
 
-<h2>Sticky Image: Scroll Down to See the Effect</h2>
-<p>The image will "stick" to the screen when you reach its scroll position.</p>
-<img class="sticky${id}" src="${img}" alt="Avatar">
-<h2>Scroll Down</h2>
-<p>Some example text..</p>
-<p><b>Scroll back up again to "remove" the sticky position.</b></p>
-<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. </p>
-  
+<div class="coupon">
+  <div class="container">
+    <h3>Company Logo</h3>
+  </div>
+  <img src="/w3images/hamburger.jpg" alt="Avatar" style="width:100%;">
+  <div class="container" style="background-color:white">
+    <h2><b>20% OFF YOUR PURCHASE</b></h2> 
+    <p>Lorem ipsum dolor sit amet, et nam pertinax gloriatur. Sea te minim soleat senserit, ex quo luptatum tacimates voluptatum, salutandi delicatissimi eam ea. In sed nullam laboramus appellantur, mei ei omnis dolorem mnesarchum.</p>
+  </div>
+  <div class="container">
+    <p>Use Promo Code: <span class="promo">BOH232</span></p>
+    <p class="expire">Expires: Jan 03, 2017</p>
+  </div>
+</div>
+ 
+ 
+ 
+ 
+ 
+ 
+ 
 </div>
 </div>
 

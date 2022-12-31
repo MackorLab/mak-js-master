c9b18210828d5358f9bba6d24f97cf2edb7fd069
MackorLab <76862637+MackorLab@users.noreply.github.com>
2023-01-01 00:59:58 +0700

    Update Header.js

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -7,7 +7,21 @@ class Header {
         const html = `
             <div class="header-container">
                 <div class="header-counter" onclick="headerPage.handlerOpenShoppingPage();">
-                    <a class="header-icon"><img src="https://i.ibb.co/xjQM8kr/shoppingcart-to-compra-12829.png" alt="shoppingcart" border="0"></a> ${count}
+                    <a class="header-icon"><img src="https://i.ibb.co/xjQM8kr/shoppingcart-to-compra-12829.png" alt="shoppingcart" border="0"></a> 
+                    
+                    
+                    
+                     <button class="btn active" onclick="filterSelection('all')"> Показать всё</button>
+                     <button class="btn" onclick="filterSelection('sytetld')"> Создание сайта</button>
+                     <button class="btn" onclick="filterSelection('ukrashvk')"> Украшение аккаунта ВК</button>
+                   
+                    
+                    
+                    
+                    
+                    ${count}
+                    
+              
                 </div>
             </div>
         `;

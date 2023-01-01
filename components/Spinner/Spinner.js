0d4f5ccc5a9b59d06d3bee38996204cdc03bef33
MackorLab <76862637+MackorLab@users.noreply.github.com>
2023-01-02 04:46:40 +0700

    Update Spinner.js

--- components/Spinner/Spinner.js
+++ components/Spinner/Spinner.js
@@ -1,21 +1,84 @@
-class Spinner {
-    handleClear() {
-        ROOT_SPINNER.innerHTML = '';
+function myFunction(x) {
+  if (x.matches) { 
+    
+    
+
+    class Spinner {
+      handleClear() {
+          ROOT_SPINNER.innerHTML = '';
+      }
+    
+      render() {
+          const html = `
+            <div class="spinner-container">
+              
+              
+           
+            <lottie-player src="https://assets3.lottiefiles.com/packages/lf20_wsccbfdk.json" background="transparent" speed="1" style="width: 200px; height: 200px;" loop="" autoplay=""></lottie-player>
+              
+            </div>
+          `;
+    
+          ROOT_SPINNER.innerHTML = html;
+      }
     }
 
-    render() {
-        const html = `
-        	<div class="spinner-container">
-            
-            
-         
-          <lottie-player src="https://assets3.lottiefiles.com/packages/lf20_wsccbfdk.json" background="transparent" speed="1" style="width: 200px; height: 200px;" loop="" autoplay=""></lottie-player>
-            
-        	</div>
-        `;
 
-        ROOT_SPINNER.innerHTML = html;
+
+
+
+
+
+
+
+
+
+
+
+  } else {
+     
+    
+
+
+    class Spinner {
+      handleClear() {
+          ROOT_SPINNER.innerHTML = '';
+      }
+    
+      render() {
+          const html = `
+            <div class="spinner-container">
+              
+              
+           
+            <div class="loader"></div>
+              
+            </div>
+          `;
+    
+          ROOT_SPINNER.innerHTML = html;
+      }
     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  }
 }
+
+var x = window.matchMedia("(max-width: 360px)")
+myFunction(x) 
+x.addListener(myFunction) 
+
 
 const spinnerPage = new Spinner();

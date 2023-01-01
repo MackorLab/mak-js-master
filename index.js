187a6f79e4c009a6b463a3f8929e731cb08ebec5
MackorLab <76862637+MackorLab@users.noreply.github.com>
2023-01-01 18:48:00 +0700

    Update index.js

--- index.js
+++ index.js
@@ -33,9 +33,9 @@ fetch('https://skyauto.me/cllbck/217669590/1685371/aVZoRWlHQmdteS8yZitXajNjWFBGU
 const htmlbtn = `
                <div id="myBtnContainer">       
                     
-                     <button class="btn active" onclick="filterSelection()"> Показать всё</button>
-                     <button class="btn" onclick="filter()"> Создание сайта</button>
-                     <button class="btn" onclick="filterSelection()"> Украшение аккаунта ВК</button>
+                     <button class="btn active" onclick="filterob()"> Показать всё</button>
+                     <button class="btn" onclick="filters()"> Создание сайта</button>
+                     <button class="btn" onclick="filtervk()"> Украшение аккаунта ВК</button>
                    
                     
                 </div>    

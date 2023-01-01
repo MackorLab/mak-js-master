2deda090e8c866a1ae1659ab2e6552cd0db820f2
MackorLab <76862637+MackorLab@users.noreply.github.com>
2023-01-01 22:18:41 +0700

    Update Spinner.js

--- components/Spinner/Spinner.js
+++ components/Spinner/Spinner.js
@@ -9,8 +9,7 @@ class Spinner {
             
             
          
-          <lottie-player src="https://assets10.lottiefiles.com/packages/lf20_d29eunxw.json" background="transparent" speed="1" style="width: 140px; height: 140px;" loop="" autoplay=""></lottie-player>  
-            
+          <div class="loader"></div>
             
         	</div>
         `;

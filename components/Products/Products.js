f6927f8050f56170fff8543f126dbfdb56904db3
MackorLab <76862637+MackorLab@users.noreply.github.com>
2023-01-02 14:12:31 +0700

    Update Products.js

--- components/Products/Products.js
+++ components/Products/Products.js
@@ -128,11 +128,11 @@ overflow: scroll;
 
   <img src="${upimg}" alt="Avatar" style="width:100%;">
   <div class="container" style="background-color:white">
-    <h2 style="color:red;"><b>${uplog}</b></h2> 
-    <p style="color:red;"> ${uptext}</p>
+    <h2 style="color:#a247fc;"><b>${uplog}</b></h2> 
+    <p style="color:#a247fc;"> ${uptext}</p>
   </div>
   <div class="container">
-    <p style="color:#ffffff;"> ${uptext_pr}<span class="promo">${price} ₽</span></p>
+    <p style="color:#ffffff;"> ${uptext_pr} <span class="promo"> ${price} ₽</span></p>
     
   </div>
 </div>

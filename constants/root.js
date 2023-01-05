f1b6ed89fee3a9410fe9a6b55b514c1bb666c981
MackorLab <76862637+MackorLab@users.noreply.github.com>
2023-01-06 05:05:55 +0700

    Update root.js

--- constants/root.js
+++ constants/root.js
@@ -4,3 +4,5 @@ const ROOT_SHOPPING = document.getElementById('shopping');
 const ROOT_SPINNER = document.getElementById('spinner');
 const ROOT_ERROR = document.getElementById('error');
 const ROOT_BTN = document.getElementById('btns');
+const ROOT_AVTOR = document.getElementById('avtors');
+

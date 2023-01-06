c87f4730baa8b2056960d4323de02c181f87ca65
MackorLab <76862637+MackorLab@users.noreply.github.com>
2023-01-07 05:25:16 +0700

    Update Header.js

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -19,7 +19,20 @@ class Header {
                
                
                
- 
+  <ul class="navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Features</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Pricing</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link disabled" href="#">Disabled</a>
+      </li>
+    </ul>
                
                
                

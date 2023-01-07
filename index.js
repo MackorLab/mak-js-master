271755317d3d4da61e934db2fa6e5a3c60c1473f
MackorLab <76862637+MackorLab@users.noreply.github.com>
2023-01-07 14:29:49 +0700

    Update index.js

--- index.js
+++ index.js
@@ -30,26 +30,3 @@ fetch('https://skyauto.me/cllbck/217669590/1685371/aVZoRWlHQmdteS8yZitXajNjWFBGU
 
 
 
-const htmlbtn = `
-             
-	     
-	       <div id="myBtnContainer">       
-                   
-                     <button class="btn active" onclick="filterob()"> Показать всё</button>
-                     <button class="btn" onclick="filterst()"> Разработка сайтов</button>
-                     <button class="btn" onclick="filtervk()"> Упаковка соц.сетей [SMM]</button>
-                    <button class="btn" onclick="filteravt()"> Об авторе 🔍 </button>
-		    
-                    
-                </div>  
-               
-               
-               
-               
-                     
-	     
-	     
-                    
-        `;
-
-        ROOT_BTN.innerHTML = htmlbtn;
